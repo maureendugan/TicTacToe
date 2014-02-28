2880dc07dc01b62e7ce4ddc4fd5fbb4551c3a573
Maureen Dugan & Raouf Nabi <student@epicodus.com>
2014-02-27 17:28:13 -0800

    as far as we got

--- js/scripts.js
+++ js/scripts.js
@@ -5,29 +5,51 @@ var Player = {
     return player;
   },
   initialize: function(name) {
+
     this.name = name;
   }
 };
 
 var Space = {
-  
+  all: [],
   create: function(x,y) {
-  var space = Object.create(Space);
-  this.xCoordinate = x;
-  this.yCoordinate = y;
-  return space;
- },
- 
- claim: function(playerObject) {
-  this.markedBy = playerObject;
- },
- 
- find: function(x,y){
-  if (Space.xCoordinate === x && Space.yCoordinate === y) {
-    return Space;
+    var spaceInstance = Object.create(Space);
+    spaceInstance.initialize(x,y);
+    Space.all.push(spaceInstance);
+    return spaceInstance;
+  }, 
+  initialize: function(x,y) {
+    this.xCoordinate = x;
+    this.yCoordinate = y;
+  },
+   claim: function(playerObject) {
+    this.markedBy = playerObject;
+  },
+  find: function(x,y){
+    if (this.xCoordinate === x && this.yCoordinate === y) {
+      return this;
+    }
   }
+};
+var Board = {
+  create: function() {
+    var board = Object.create(Board);
+    board.initialize();
+    return board;
+  },
+   initialize: function() {
+     for (var x=1; x<=3; x++) {
+        for (var y=1; y<=3; y++) {
+         Space.create(x,y);
+        }
+      } 
+   },
+   testForWin: function() {
+//write a test that will at least compare the x coordinates of the spaces claimed by TestPlayer
+you need to use:
+Space.all[i].markedBy.name find where the name is the same and start comparing
+    return true;
+   }
+};
 
- }
 
-
-}
--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,7 @@
+beforeEach(function() {
+  Board.spaces = [];
+  Space.all = [];
+});
 describe('Player', function() {
   describe('create', function() {
     it('creates a new instance of a player', function () {
@@ -7,7 +11,7 @@ describe('Player', function() {
     it('creates a player with a name', function() {
       var testPlayer = Player.create('Tony');
       testPlayer.name.should.equal('Tony');
-    })
+    });
   });
   describe('initialize', function() {
     it('Gives a name to the player', function () {
@@ -17,20 +21,33 @@ describe('Player', function() {
     });
   });
 });
-describe('Space', function(){
+describe('Space', function() {
   describe('create', function() {
     it('creates a new instance of a space', function () {
       var testSpace = Space.create();
       Space.isPrototypeOf(testSpace).should.equal(true); 
-  });
+    });
 
-  it('gives the space x and y coordinates', function () {
-      var testSpace = Space.create(3, 1);
-      testSpace.xCoordinate.should.equal(3);
-      testSpace.yCoordinate.should.equal(1);
+    it('gives the space x and y coordinates', function () {
+        var testSpace = Space.create(3, 1);
+        testSpace.xCoordinate.should.equal(3);
+        testSpace.yCoordinate.should.equal(1);
+      });
+    it('add any created space to the .all property', function() {
+      var testSpace = Space.create(3,4);
+      var testSpace2 = Space.create(5,6);
+      Space.all.should.eql([testSpace, testSpace2]);
     });
   });
-  describe('claim', function(){
+  describe('intitalize', function() {
+    it('gives the space x and y coordinates', function() {
+      var testSpace = Object.create(Space);
+      testSpace.initialize(1,2);
+      testSpace.xCoordinate.should.equal(1);
+      testSpace.yCoordinate.should.equal(2);
+    });
+  });
+  describe('claim', function() {
     it('Lets a player mark a space', function() {
       var testPlayer = Player.create('Tony');
       var testSpace = Space.create(1,2);
@@ -41,7 +58,38 @@ describe('Space', function(){
   describe('find', function() {
     it('returns the Space if the coordinates match', function() {
       var testSpace = Space.create(3,2);
-      Space.find(3, 2).should.equal(Space);
+      testSpace.find(3,2).should.equal(testSpace);
+    });
+  });
+});
+
+describe('Board', function() {
+  describe('create', function() {
+    it('creates the object Board', function() {
+      var testBoard = Board.create();
+      Board.isPrototypeOf(testBoard).should.equal(true);
+    });
+    it('creates 9 spaces when created', function() {
+      var testBoard = Board.create();
+      Space.all.length.should.equal(9);
+      Space.all[0].xCoordinate.should.equal(1);
+    });
+  });
+  describe('initialize', function() {
+    it('starts a board with 9 spaces', function() {
+      var testBoard = Object.create(Board);
+      testBoard.initialize();
+      Space.all.length.should.equal(9);
+    });
+  });
+  describe('testForWin', function() {
+    it('returns true if the player x has marked 3 spaces in a row', function() {
+      var testBoard = Board.create();
+      var testPlayer = Player.create("Tony");
+      Space.all[3].claim(testPlayer);
+      Space.all[4].claim(testPlayer);
+      Space.all[5].claim(testPlayer);
+      Board.testForWin().should.equal(true);
     });
   });
 });
